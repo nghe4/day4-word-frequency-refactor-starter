97a5b604c6a4643f3de6d6890be0424fe5ee587d
nghe4 <nghe4@oocl.com>
2025-09-04 17:42:55 +0800

    refactor: remove unnecessary import

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
